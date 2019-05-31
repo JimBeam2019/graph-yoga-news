9c9a5126381e4477f298ee4a7fa994d812fa2273
JimBeam2019 <bingking68@hotmail.com>
2019-05-31 10:04:11 +0800

    Added the connection of Prisma.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,8 @@ import { links } from '../data/links';
 
 const info = () => `API of a news clone`;
 
-const feed = () => links;
+// const feed = () => links;
+const feed = (root, args, context, info) => context.prisma.links();
 
 const link = (parent, args) => links.find(linkObj => linkObj.id === args.id);
 
