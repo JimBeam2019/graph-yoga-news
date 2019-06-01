4c2a60cd0a2304c835c38a4c757aa34e60e515ef
JimBeam2019 <bingking68@hotmail.com>
2019-06-01 21:47:36 +0800

    User authentication.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,8 +1,22 @@
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { 
+    APP_SECRET,
+    getUserId,
+ } from '../utils';
 
-const post = (parent, args, context, info) => context.prisma.createLink({
-    url: args.url,
-    description: args.description,
-});
+const post = (parent, args, context, info) => {
+    const userId = getUserId(context);
+    return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+        postedBy: {
+            connect: {
+                id: userId
+            }
+        },
+    });
+};
 
 const updateNews = (parent, args, context, info) => context.prisma.updateLink({
     where: { id: args.id },
@@ -13,8 +27,49 @@ const deleteNews = (parent, args, context, info) => context.prisma.deleteLink({
     id: args.id,
 });
 
+async function signup(parent, args, context, info) {
+    const password = await bcryptjs.hash(args.password, 10);
+    const user = await context.prisma.createUser({
+        ...args,
+        password,
+    });
+    const token = jwt.sign({
+        userId: user.id,
+    }, APP_SECRET);
+
+    return {
+        token,
+        user,
+    };
+}
+
+async function login(parent, args, context, info) {
+    const user = await context.prisma.user({
+        email: args.email,
+    });
+    if (!user) {
+        throw new Error('No such user found.');
+    }
+
+    const valid = await bcryptjs.compare(args.password, user.password);
+    if (!valid) {
+        throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({
+        userId: user.id,
+    }, APP_SECRET);
+
+    return {
+        token,
+        user,
+    };
+}
+
 module.exports = {
     post,
     updateNews,
     deleteNews,
+    signup,
+    login,
 };
---
+++ src/utils.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const APP_SECRET = 'GraphQL*pw2019';
+
+const getUserId = (context) => {
+    const Authorization = context.request.get('Authorization');
+
+    if (Authorization) {
+        const token = Authorization.replace('Bearer ', '');
+        const {
+            userId
+        } = jwt.verify(token, APP_SECRET);
+
+        return userId;
+    }
+
+    throw new Error('Not authenticated.');
+};
+
+module.exports = {
+    APP_SECRET,
+    getUserId,
+};
